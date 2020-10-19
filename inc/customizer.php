2cf081d8f909cf77bf5b263c105a59b1cd8e8f0a
Claire Koga <ckoga1@my.bcit.ca>
2020-10-19 10:39:02 -0700

    Set up files

---
+++ inc/customizer.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Meal Delivery Theme Customizer
+ *
+ * @package Meal_Delivery
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function meal_delivery_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial(
+			'blogname',
+			array(
+				'selector'        => '.site-title a',
+				'render_callback' => 'meal_delivery_customize_partial_blogname',
+			)
+		);
+		$wp_customize->selective_refresh->add_partial(
+			'blogdescription',
+			array(
+				'selector'        => '.site-description',
+				'render_callback' => 'meal_delivery_customize_partial_blogdescription',
+			)
+		);
+	}
+}
+add_action( 'customize_register', 'meal_delivery_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function meal_delivery_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function meal_delivery_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function meal_delivery_customize_preview_js() {
+	wp_enqueue_script( 'meal-delivery-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
+}
+add_action( 'customize_preview_init', 'meal_delivery_customize_preview_js' );
