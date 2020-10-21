89401a754afb6308c3da88989976a0f6db2186ab
Claire Koga <ckoga1@my.bcit.ca>
2020-10-21 11:39:01 -0700

    Edited the code for the testimonials

--- inc/cpt-taxonomy.php
+++ inc/cpt-taxonomy.php
@@ -5,30 +5,20 @@ function md_register_custom_post_types() {
     //register Testimonials CPT
 
     $labels = array(
-        'name'               => _x( 'Testimonials', 'post type general name' ),
-        'singular_name'      => _x( 'Testimonial', 'post type singular name'),
-        'menu_name'          => _x( 'Testimonials', 'admin menu' ),
-        'name_admin_bar'     => _x( 'Testimonials', 'add new on admin bar' ),
-        'add_new'            => _x( 'Add New', 'Testimonials' ),
-        'add_new_item'       => __( 'Add New Testimonials' ),
+        'name'               => _x( 'Testimonials', 'post type general name'  ),
+        'singular_name'      => _x( 'Testimonial', 'post type singular name'  ),
+        'menu_name'          => _x( 'Testimonials', 'admin menu'  ),
+        'name_admin_bar'     => _x( 'Testimonial', 'add new on admin bar' ),
+        'add_new'            => _x( 'Add New', 'testimonial' ),
+        'add_new_item'       => __( 'Add New Testimonial' ),
         'new_item'           => __( 'New Testimonial' ),
         'edit_item'          => __( 'Edit Testimonial' ),
-        'view_item'          => __( 'View Testimonial' ),
+        'view_item'          => __( 'View Testimonial'  ),
         'all_items'          => __( 'All Testimonials' ),
         'search_items'       => __( 'Search Testimonials' ),
         'parent_item_colon'  => __( 'Parent Testimonials:' ),
-        'not_found'          => __( 'No Testimonial found.' ),
-        'not_found_in_trash' => __( 'No Testimonial found in Trash.' ),
-        'archives'           => __( 'Testimonials Archives'),
-        'insert_into_item'   => __( 'Insert into Testimonial'),
-        'uploaded_to_this_item' => __( 'Uploaded to this Testimonial'),
-        'filter_item_list'   => __( 'Filter Testimonial list'),
-        'items_list_navigation' => __( 'Testimonials list navigation'),
-        'items_list'         => __( 'Testimonials list'),
-        'featured_image'     => __( 'Testimonials feature image'),
-        'set_featured_image' => __( 'Set Testimonials feature image'),
-        'remove_featured_image' => __( 'Remove Testimonials feature image'),
-        'use_featured_image' => __( 'Use as feature image'),
+        'not_found'          => __( 'No testimonials found.' ),
+        'not_found_in_trash' => __( 'No testimonials found in Trash.' ),
     );
 
     $args = array(
@@ -37,21 +27,18 @@ function md_register_custom_post_types() {
         'publicly_queryable' => true,
         'show_ui'            => true,
         'show_in_menu'       => true,
-        'show_in_nav_menus'  => true,
-        'show_in_admin_bar'  => true,
         'show_in_rest'       => true,
         'query_var'          => true,
         'rewrite'            => array( 'slug' => 'testimonials' ),
         'capability_type'    => 'post',
-        'has_archive'        => true,
+        'has_archive'        => false,
         'hierarchical'       => false,
-        'menu_position'      => 5,
-        'menu_icon'          => 'dashicons-archive',
+        'menu_position'      => 7,
+        'menu_icon'          => 'dashicons-heart',
         'supports'           => array( 'title', 'editor' ),
-        'template'           => array( 'core/quotes' ),
-        'template_lock'      => 'all',
+        'template'           => array( array( 'core/quote' ) ),
+        'template_lock'      => 'all',                              
     );
-
     register_post_type( 'md-testimonials', $args );
     
 // register recipes cpt
