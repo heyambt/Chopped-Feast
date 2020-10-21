db826e4ab7166ec8cfa0c0f1f7a0830919942325
Claire Koga <ckoga1@my.bcit.ca>
2020-10-21 10:57:34 -0700

    Created template for the home page.

--- front-page.php
+++ front-page.php
@@ -17,22 +17,29 @@ get_header();
 
 	<main id="primary" class="site-main">
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+		<?php while ( have_posts() ) :
+				the_post(); ?>
 
-			get_template_part( 'template-parts/content', 'page' );
+		<section class="hero-image"></section>
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+		<section class="about-us">
+			<h2>About Us</h2>
+			<?php the_content(); ?>
+		</section>
 
-		endwhile; // End of the loop.
-		?>
+		<section class="how-it-works">
+			<h2>How It Works</h2>
+		</section>
 
+		<section class="testimonials"><!--testimonial array--></section>
+
+		<section class="social-proof"></section>
+
+		<section class="call-to-action"></section>
+
+		
+		<?php endwhile; // End of the loop. ?>
 	</main><!-- #main -->
 
 <?php
-get_sidebar();
 get_footer();
