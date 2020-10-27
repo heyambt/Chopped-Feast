7a54f1023df7405d4d91e6c448d84c3afb8adc39
heyam <hiam.bt@gmail.com>
2020-10-27 16:29:17 -0700

    js added to functions

--- functions.php
+++ functions.php
@@ -145,9 +145,12 @@ function meal_delivery_scripts() {
 
 	wp_enqueue_script( 'meal-delivery-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
 
+	wp_enqueue_script( 'meal-delivery-menu', get_template_directory_uri() . '/js/menu.js', array(), _S_VERSION, true ); 
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
+	
 }
 add_action( 'wp_enqueue_scripts', 'meal_delivery_scripts' );
 
--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,7 @@
 //get elements
 const selectElement = document.querySelector('.dropdown');
 selectElement.addEventListener('change', (event) => {
-  var elements = document.querySelectorAll('week-section');
+  var elements = document.querySelectorAll('.week-section');
   for (var i= 0; i < elements.length; i++) {
       if ( elements[i].classList.contains( event.target.value ) ) {
         elements[i].classList.add('visible');
