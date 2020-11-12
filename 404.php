993e84b96cc43db1bb4efd7b477e0b84900b5767
heyam <hiam.bt@gmail.com>
2020-11-12 13:40:49 -0800

    404 page styled

--- 404.php
+++ 404.php
@@ -14,41 +14,43 @@ get_header();
 
 		<section class="error-404 not-found">
 			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'meal-delivery' ); ?></h1>
+				<h1 class="page-title"><?php esc_html_e( 'Oops! Looks like you took a wrong turn!', 'meal-delivery' ); ?></h1>
 			</header><!-- .page-header -->
 
 			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'meal-delivery' ); ?></p>
+				<p><?php esc_html_e( 'Please Try search bar or click on Home. ', 'meal-delivery' ); ?></p>
 
 					<?php
 					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
+					// the_widget( 'WP_Widget_Recent_Posts' );
 					?>
+					<section class="call-to-action">
+					<a href="<?php echo get_permalink(22);?>">Home</a>		
+					</section>
 
-					<div class="widget widget_categories">
+					<!-- <div class="widget widget_categories">
 						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'meal-delivery' ); ?></h2>
-						<ul>
+						<ul> -->
 							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
+							// wp_list_categories(
+							// 	array(
+							// 		'orderby'    => 'count',
+							// 		'order'      => 'DESC',
+							// 		'show_count' => 1,
+							// 		'title_li'   => '',
+							// 		'number'     => 10,
+							// 	)
+							// );
+							// ?>
 						</ul>
 					</div><!-- .widget -->
 
 					<?php
 					/* translators: %1$s: smiley */
-					$meal_delivery_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'meal-delivery' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$meal_delivery_archive_content" );
+					// $meal_delivery_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'meal-delivery' ), convert_smilies( ':)' ) ) . '</p>';
+					// the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$meal_delivery_archive_content" );
 
-					the_widget( 'WP_Widget_Tag_Cloud' );
+					// the_widget( 'WP_Widget_Tag_Cloud' );
 					?>
 
 			</div><!-- .page-content -->
