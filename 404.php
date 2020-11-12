c9ba488903e5232cb583d77e16608ded1450c38c
heyam <hiam.bt@gmail.com>
2020-11-12 15:02:43 -0800

    buttons styles changed to large

--- 404.php
+++ 404.php
@@ -18,43 +18,21 @@ get_header();
 			</header><!-- .page-header -->
 
 			<div class="page-content">
-				<p><?php esc_html_e( 'Please Try search bar or click on Home. ', 'meal-delivery' ); ?></p>
+				<p><?php esc_html_e( 'Please click on Home. ', 'meal-delivery' ); ?></p>
 
-					<?php
-					get_search_form();
-					// the_widget( 'WP_Widget_Recent_Posts' );
-					?>
+				<?php
+				echo wp_get_attachment_image()
+					
+				?>
+					
 					<section class="call-to-action">
 					<a href="<?php echo get_permalink(22);?>">Home</a>		
 					</section>
 
-					<!-- <div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'meal-delivery' ); ?></h2>
-						<ul> -->
-							<?php
-							// wp_list_categories(
-							// 	array(
-							// 		'orderby'    => 'count',
-							// 		'order'      => 'DESC',
-							// 		'show_count' => 1,
-							// 		'title_li'   => '',
-							// 		'number'     => 10,
-							// 	)
-							// );
-							// ?>
-						</ul>
-					</div><!-- .widget -->
+					
 
-					<?php
-					/* translators: %1$s: smiley */
-					// $meal_delivery_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'meal-delivery' ), convert_smilies( ':)' ) ) . '</p>';
-					// the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$meal_delivery_archive_content" );
-
-					// the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
 
 			</div><!-- .page-content -->
-		</section><!-- .error-404 -->
 
 	</main><!-- #main -->
 
